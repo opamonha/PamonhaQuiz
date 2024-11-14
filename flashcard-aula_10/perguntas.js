6817371d83e379225cd559ffafde521ea2ca87aa
opamonha <goncalvescaio0411@gmail.com>
2024-11-14 07:28:29 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o único time brasileiro fora de uma capital, a conquistar uma libertadores?',
+    'Santos Futebol Clube'
+)
+
+criaCartao(
+    'Anime',
+    'Qual anime tem um "mundo de pedra"?',
+    'Dr. Stone'
+)
+
+criaCartao(
+    'Música',
+    'Quem fez a música "vem e brota aqui na base"?',
+    'MC Doguinha'
+)
+
+criaCartao(
+    'Música',
+    'Complete: "E toda vez que você me disser..."',
+    'Oi)'
+)
